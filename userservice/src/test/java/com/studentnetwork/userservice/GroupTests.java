b67e675a91aba98d2246850fd27b3c253f55ff35
Jazz-NZ <56455946+Jazz-NZ@users.noreply.github.com>
2022-09-29 20:26:05 +0200

    Test for group

---
+++ userservice/src/test/java/com/studentnetwork/userservice/GroupTests.java
@@ -0,0 +1,29 @@
+package com.studentnetwork.userservice;
+
+import com.studentnetwork.userservice.service.AppUserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+public class GroupTests {
+    @Autowired
+    AppUserService userService;
+
+    @Test
+    void saveGroupEmptyParams(){
+        Assertions.assertThrows(Exception.class, () -> {userService.saveGroup("","");});
+    }
+
+    @Test
+    void saveGroupEmptyUsername(){
+        Assertions.assertThrows(Exception.class, () -> {userService.saveGroup("randomrandom","");});
+    }
+
+    @Test
+    void saveGroupEmptyGroupName(){
+        Assertions.assertThrows(Exception.class, () -> {userService.saveGroup("","john");});
+    }
+}
